9dcbf503c577ee219950105f56c1d0e3a4b76bd0
Your Name <you@example.com>
2025-09-03 11:56:34 +0330

    add is admin add is user

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 from flask import Flask
 from blueprints.genral import genral
+from blueprints.admin import  admin
+from blueprints.user import  user
+
 
 app = Flask(__name__)
 app.register_blueprint(genral)
+app.register_blueprint(admin)
+app.register_blueprint(user)
+
 
 if __name__ == '__main__':
     app.run()
---
+++ blueprints/admin.py
@@ -0,0 +1,8 @@
+from flask import Blueprint
+
+admin = Blueprint("admin", __name__)
+
+
+@admin.route('/admin')
+def index():
+    return 'this is admin page'
